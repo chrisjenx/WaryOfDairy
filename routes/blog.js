3f48057124f79fdd86cb4fba66e4143342d532b1
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2014-07-27 01:48:55 +0100

    loading blog posts

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * @param db {required} the monk db object
+ * @param func {required} for callbacks of results callback(err, docs){err, array of posts}
+ */
+function getAllPosts(db, func) {
+  db.get('posts').find({}, {}, function (err, docs) {
+    if (err) func(err, null);
+    return func(null, docs);
+  });
+}
+
 /* GET users listing. */
 router.get('/', function (req, res) {
-//  res.render('index', {});
-  res.render('respond with a resource');
+  getAllPosts(req.db, function (err, posts) {
+    res.render('blog/index', {
+      posts: posts
+    });
+  });
+});
+
+
+// TODO auth
+
+/* GET create blog */
+router.get('/create', function (req, res) {
+
 });
 
 module.exports = router;
